b8ab0d9a9eca38a70c4665574295302d8bf61ccb
Василь Картичак <w.i.k.mailua@gmail.com>
2023-10-31 20:21:09 +0200

    fastapi - 12

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 #  WEB
 
